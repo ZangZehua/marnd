5cad5b55a3bf5d989dad81fa2568fe929eb60d91
ZangZehua <zhzang97@gmail.com>
2022-09-05 09:16:50 +0800

    check

--- src/controllers/immac_controller.py
+++ src/controllers/immac_controller.py
@@ -20,13 +20,8 @@ class ImmacMAC:
 
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
         # Only select actions for the selected batch elements in bs
-        print("************************in select actions")
         avail_actions = ep_batch["avail_actions"][:, t_ep]
         agent_outputs = self.forward(ep_batch, t_ep, train_mode=False)
-        print("******************")
-        print(agent_outputs[bs], agent_outputs[bs].shape)
-        print(avail_actions[bs], avail_actions[bs].shape)
-        print("******************")
         chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
         return chosen_actions
 
